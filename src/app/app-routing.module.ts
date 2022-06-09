276c571b1e45f3290acb9758d92526129ab38f5c
Paulina Torres <iptorresb25@gmail.com>
2022-06-08 21:12:27 -0600

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { CustomersComponent } from './customers/customers.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: "users",
+  component: UsersComponent
+},
+{
+  path: "customers",
+  component: CustomersComponent
+},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
+import { UsersComponent } from './users/users.component';
+import { CustomersComponent } from './customers/customers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    NavbarComponent,
+    LoginComponent,
+    UsersComponent,
+    CustomersComponent
   ],
   imports: [
     BrowserModule,
